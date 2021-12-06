9e098acdd1084c004ec29b2652e958a2b37b2e27
katerineN <Katush-ka2000@yandex.ru>
2021-12-06 23:18:49 +0300

    есть примерно ничего, но мы не расстраиваемся

--- CornishRoom/Form1.cs
+++ CornishRoom/Form1.cs
@@ -12,6 +12,21 @@ namespace CornishRoom
 {
     public partial class Form1 : Form
     {
+        //перемножение матриц
+        public static float[,] MultMatrix(float[,] m1, float[,] m2)
+        {
+            float[,] res = new float[m1.GetLength(0), m2.GetLength(1)];
+
+            for (int i = 0; i < m1.GetLength(0); ++i)
+            for (int j = 0; j < m2.GetLength(1); ++j)
+            for (int k = 0; k < m2.GetLength(0); k++)
+            {
+                res[i, j] += m1[i, k] * m2[k, j];
+            }
+
+            return res;
+        }
+        
         public Form1()
         {
             InitializeComponent();
--- CornishRoom/Point3D.cs
+++ CornishRoom/Point3D.cs
@@ -24,21 +24,25 @@ namespace CornishRoom
             return new Point((int)X, (int)Y);
         }
         
+        //расстояние между точками
         public float DistanceTo(Point3D p2)
         {
             return (float)Math.Sqrt((X - p2.X) * (X - p2.X) + (Y - p2.Y) * (Y - p2.Y) + (Z - p2.Z) * (Z - p2.Z));
         }
         
+        //длина вектора
         public float Length()
         {
             return (float)Math.Sqrt(X * X + Y * Y + Z * Z);
         }
         
+        //скалярное произведение
         public static float Scalar(Point3D p1, Point3D p2)
         {
             return p1.X * p2.X + p1.Y * p2.Y + p1.Z * p2.Z;
         }
 
+        //нормализация
         public static Point3D Normalize(Point3D p)
         {
             float z = (float)Math.Sqrt((float)(p.X * p.X + p.Y * p.Y + p.Z * p.Z));
@@ -57,6 +61,7 @@ namespace CornishRoom
             return new Point3D(point1.X - point2.X, point1.Y - point2.Y, point1.Z - point2.Z);
         }
         
+        //векторное произведение векторов
         public static Point3D operator *(Point3D p1, Point3D p2)
         {
             return new Point3D(p1.Y * p2.Z - p1.Z * p2.Y, p1.Z * p2.X - p1.X * p2.Z, p1.X * p2.Y - p1.Y * p2.X);
